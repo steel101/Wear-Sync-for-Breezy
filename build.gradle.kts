1cee9d9306f91b76d9bb22af74e9bd62af278709
steel johnson <steelvargas@gmail.com>
2026-07-02 22:33:41 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,4 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
-android {
-    dependenciesInfo {
-        // Disables dependency metadata when building APKs (for IzzyOnDroid/F-Droid)
-        includeInApk = false
-        // Disables dependency metadata when building Android App Bundles (for Google Play)
-        includeInBundle = false
-    }
-}
-
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
