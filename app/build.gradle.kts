0dda8f181e1a47ec94f9d9fa38a60f28657cbfd6
steel johnson <steelvargas@gmail.com>
2026-07-11 00:25:28 -0400

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.steel101.wearsyncforbreezy"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
-        versionName = "1.0.36"
+        versionCode = 2
+        versionName = "1.0.37"
         resConfigs("en")
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
