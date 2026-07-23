edad9bc8299cf15986b2e08193dcb572c75bff61
steel johnson <steelvargas@gmail.com>
2026-07-23 02:07:17 -0400

    update app ver

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         minSdk = 26
-        buildConfigField("int", "VERSION_CODE", "29")
+        buildConfigField("int", "VERSION_CODE", "30")
     }
 
     buildFeatures {
--- wear/build.gradle.kts
+++ wear/build.gradle.kts
@@ -26,8 +26,8 @@ android {
         applicationId = "com.steel101.wearsyncforbreezy"
         minSdk = 26
         targetSdk = 35
-        versionCode = 29
-        versionName = "1.0.65"
+        versionCode = 30
+        versionName = "1.0.66"
         resConfigs("en")
         vectorDrawables {
             useSupportLibrary = true
